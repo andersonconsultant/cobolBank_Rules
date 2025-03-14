b851064dce16f9202ffd9ebec7dc5179723db086
andersonconsultant <andersonsilvads26@gmail.com>
2025-03-14 12:40:23 -0300

    h0 just api
    h0 clean just api

--- temp/api/cobol/teste.cob
+++
@@ -1,5 +0,0 @@
-       01  CONNECTION-STRING PIC X(50) VALUE "host=localhost dbname=test".
-       PROCEDURE DIVISION.
-           DISPLAY "Conectando ao banco de dados...".
-           CALL "PQconnectdb" USING CONNECTION-STRING.
-           STOP RUN.
--- temp/api/teste/outros/connectbd.cob
+++
@@ -1,9 +0,0 @@
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID. CONNECTBD.
-       DATA DIVISION.
-       WORKING-STORAGE SECTION.
-       01  CONNECTION-STRING PIC X(50) VALUE "host=localhost dbname=test".
-       PROCEDURE DIVISION.
-           DISPLAY "Conectando ao banco de dados...".
-           CALL "PQconnectdb" USING CONNECTION-STRING.
-           STOP RUN.
